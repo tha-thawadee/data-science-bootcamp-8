dca094bf6e8a1a305e3f886859cc0fe16f425674
thawadee.t@gmail.com <thawadee.t@gmail.com>
2023-08-05 02:56:47 +0000

    add new file

---
+++ data_viz.r
@@ -0,0 +1,4 @@
+# load pplot library
+
+library(ggplot2)
+ggplot(diamonds, aes(caret, price)) + geom_plot()
--- hello_world.r
+++ hello_world.r
@@ -5,5 +5,9 @@ print("GitHub is very good")
 
 library(dplyr)
 mtcars %>%
-select(1:5) %>%
-filter(mpg > 30)
+  select(1:5) %>%
+  filter(mpg > 30)
+
+mtcars %>%
+  select(mpg) %>%
+  filter(mpg > 30)
